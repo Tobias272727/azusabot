4b189794b93046d5c4f2bed538268599019b39d5
tobias272727 <siping123@163.com>
2020-05-24 14:52:05 +0800

    image spider to pixivic

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from nonebot.default_config import *
 
-HOST = '127.0.0.0'
+HOST = '127.0.0.1'
 PORT = 9222
-
-SUPERUSERS = {12345678}
+NICKNAME = {'azusa','阿梓喵','梓','中野梓'}
+SUPERUSERS = {353152858}
 COMMAND_START = {'', '/', '!', '／', '！'}
